fbff7670577a34cfcb57ff9a15f891f00b209de6
mjcatanz <catanzaromj@gmail.com>
2020-10-02 19:31:39 -0500

    Create auxiliary.py
    Add various programs for manipulating landscapes and diagrams.

---
+++ auxiliary.py
@@ -0,0 +1,10 @@
+"""
+Auxilary functions for working with persistence diagrams.
+"""
+
+def death_vector(diagram:list, homologica_degree:int = 0):
+    """ Returns the death vector in degree 0 for the persistence diagram.
+    
+    """
+    
+    pass
