4c1f090bd18c1f7f79c646e597129031b5bedc6f
gabbyangeloro <68562816+gabbyangeloro@users.noreply.github.com>
2020-10-08 17:30:57 -0500

    writing grid algorithm

--- PersistenceLandscapeGrid.py
+++ PersistenceLandscapeGrid.py
@@ -12,21 +12,33 @@ len_dgm = len(dgm)
 # Calculate m 
 m = int(max([x for pair in dgm for x in pair])/2)
 
-# Initialze W to a list of 2m + 1 empty lists
+# initialze W to a list of 2m + 1 empty lists
 W = [[] for i in range(2*m +1)]
 
 # for each birth death pair
-for bd in range(len_dgm):
+for bd in dgm:
     b, d = bd
     # for j=1 to j=height of (b_i, d_i) from rank function
-    for j in range(1, (bd[1]- bd[0])/2 +1):
+    for j in range(1, int((bd[1]- bd[0])/2 +1)) :
         W[b + j].append(j)
     
     # for j=1 to j=height of (b_i, d_i) +1 from rank function
-    for j in range(1, (bd[1]- bd[0])/2):
+    for j in range(1, int(((bd[1]- bd[0])/2))):
         W[d - j].append(j)
 
-# sort each 
+# sort each list in W
+for i in range(len(W)):
+    W[i] = sorted(W[i], reverse = True)
+    
+# calculate k: max length of lists in W
+K = max([len(_) for _ in W ])
+
+# initialize V
+V = []
+
+
+    
+    
             
         
         
