1a5f31b557c46c98fc77c53cdbe2c50dbaf92183
mjcatanz <catanzaromj@gmail.com>
2020-10-20 12:02:16 -0500

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -1,74 +1,50 @@
 """
 Visualization methods for plotting persistence landscapes.
 
-DO NOT USE THIS NOW. USE EXAMPLES.PY INSTEAD.
 """
 
 import matplotlib.pyplot as plt
-from PersistenceLandscape import PersistenceLandscape
-
-def plot_landscape(landscape: PersistenceLandscape):
-    """
-    A plot of the persistence landscape
-    """
-    pass
-
-
-#%%
-# This import registers the 3D projection, but is otherwise unused.
-from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
-
-from matplotlib.collections import PolyCollection
-import matplotlib.pyplot as plt
-from matplotlib import colors as mcolors
 import numpy as np
+from PersistenceLandscape import PersistenceLandscape
+from operator import itemgetter
+from matplotlib import cm
 
-# Fixing random state for reproducibility
-np.random.seed(19680801)
-
-
-def polygon_under_graph(xlist, ylist):
+def plot_landscape(landscape: PersistenceLandscape,
+                   num_steps: int = 3000,
+                   color=cm.viridis,
+                   alpha=0.8,
+                   padding: float = 0.1,
+                   depth_padding: float = 0.5):
     """
-    Construct the vertex list which defines the polygon filling the space under
-    the (xlist, ylist) line graph.  Assumes the xs are in ascending order.
+    A plot of the persistence landscape.
+    
+    Warning: This function is quite slow, especially for large landscapes.
     """
-    return [(xlist[0], 0.), *zip(xlist, ylist), (xlist[-1], 0.)]
-
-
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-
-# Make verts a list, verts[i] will be a list of (x,y) pairs defining polygon i
-verts = []
-
-# Set up the x sequence
-xs = np.linspace(0., 10., 26)
-
-# The ith polygon will appear on the plane y = zs[i]
-zs = [0] # range(4)
-
-L = np.array([[0.08088598, 0.        ],
-       [0.09222407, 0.01133809],
-       [0.10143114, 0.00213103],
-       [0.12616551, 0.0268654 ],
-       [0.13100001, 0.02203091],
-       [0.13280252, 0.02383342],
-       [0.14482516, 0.01181078],
-       [0.20296358, 0.06994919],
-       [0.27291277, 0.        ]])
-
-#for i in L:
-   # ys = np.random.rand(len(xs))
- #   verts.append(polygon_under_graph(i[0]))
-
-poly = PolyCollection(L, facecolors=['r', 'g', 'b', 'y'], alpha=.6)
-ax.add_collection3d(poly, zs=[0], zdir='y')
-
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
-ax.set_xlim(0, 10)
-ax.set_ylim(-1, 4)
-ax.set_zlim(0, 1)
-
-plt.show()
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    landscape.compute_landscape()
+    min_crit_pt = min(landscape[0], key=itemgetter(0))[0]
+    max_crit_pt = max(landscape[0], key=itemgetter(0))[0]
+    max_crit_val = max(landscape[0],key=itemgetter(1))[1]
+    domain = np.linspace(min_crit_pt-padding*0.1, max_crit_pt+padding*0.1, num=num_steps)
+    for depth, l in enumerate(landscape):
+        xs, zs = zip(*l)
+        image = np.interp(domain, xs, zs)
+        for x, z in zip(domain,image):
+            if z == 0.: continue
+            ax.plot(
+                [x,x],
+                [depth_padding*depth,depth_padding*depth],
+                [0,z],
+                linewidth=0.5,
+                alpha=alpha,
+                c=color(z/max_crit_val))
+    
+    #ax.set_xlabel('X')
+    #ax.set_ylabel('depth')
+    #ax.set_zlabel('Z')
+    #ax.set_xlim(min_crit_pt-padding, max_crit_pt+padding)
+    #ax.set_ylim(0, depth_padding*landscape.max_depth+1)
+    # ax.set_zlim(0, max_crit_val+padding)
+    ax.view_init(10,90)
+    plt.show()
