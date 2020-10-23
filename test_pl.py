3dc6f3bbb24c4c808ffec3e284e2d56420baa9b2
mjcatanz <catanzaromj@gmail.com>
2020-10-23 18:17:08 -0500

    Add unit test for PL

--- test_pl.py
+++ test_pl.py
@@ -7,24 +7,27 @@ Unit test for PersistenceLandscapeExact
 import unittest 
 import numpy as np
 
-from PersistenceLandscape import *
+from PersistenceLandscape import PersistenceLandscape
 
 class TestPersistenceLandscapeExact(unittest.TestCase):
     
-    def setUp(self):
-        self.diagrams = [np.array([[1.0, 5.0], [2.0, 8.0], 
-                           [3.0, 4.0], [5.0, 9.0], [6.0, 7.0]])]
-        self.homological_degree = 0
-        
-        P = PersistenceLandscape(self.diagrams, self.homological_degree)
-        P.compute_landscape()
-        
+#    def setUp(self):
+#        self.diagrams = [np.array([[1.0, 5.0], [2.0, 8.0], 
+#                           [3.0, 4.0], [5.0, 9.0], [6.0, 7.0]])]
+#        self.homological_degree = 0
+#        P = PersistenceLandscape(self.diagrams, self.homological_degree)
+#        P.compute_landscape()
     
-    def TestPLE(self):
+    def test_pl_critical_pairs(self):
         """
         Test example from Peter & Pavel's paper
 
         """
+        P = PersistenceLandscape(
+            diagrams=[np.array([[1.0, 5.0], [2.0, 8.0], [3.0, 4.0], [5.0, 9.0],
+                                [6.0, 7.0]])],
+            homological_degree=0)
+        P.compute_landscape()
         self.assertEqual(P.critical_pairs, [[[1.0, 0],[3.0, 2.0],
         [3.5, 1.5],[5.0, 3.0],[6.5, 1.5],[7.0, 2.0],[9.0, 0]],
         [[2.0, 0], [3.5, 1.5], [5.0, 0], [6.5, 1.5], [8.0, 0]],
