d0e76a066b058a0e2b031a1f667e0f56a59523bc
catanzaromj <catanzaromj@gmail.com>
2020-10-27 19:10:04 -0500

    Update pl_transformer.py

--- pl_transformer.py
+++ pl_transformer.py
@@ -11,19 +11,22 @@ from PersistenceLandscape import PersistenceLandscape
 # PL class.
 
 class PL_exact(BaseEstimator, TransformerMixin):
-    """ A scikit-learn transformer class for exact persistence landscapes.
+    """ A scikit-learn transformer class for exact persistence landscapes. The transform
+    method returns the list of critical pairs for the landscape. For a vectorized
+    encoding of the landscape, using the PL_grid transformer.
     """
     def __init__(self,homological_degree:int = 0):
         self.homological_degree = homological_degree
         
-    def fit(self,X,y=None):
+    def fit(self,X, y=None):
         return self
     
-    def transform(self,X,homological_degree: int, y=None):
+    def transform(self, X, y=None):
         # Add check that X is the output of a PH calculation.
         # return X[self.homological_degree].compute_landscape()
-        return PersistenceLandscape(diagrams=X, 
-                                    homological_degree=homological_degree).compute_landscape().critical_pairs()
+        result = PersistenceLandscape(diagrams=X, homological_degree=self.homological_degree)
+        result.compute_landscape()
+        return result.critical_pairs
     
 class PL_grid(BaseEstimator, TransformerMixin):
     """ A scikit-learn transformer class for approximate persistence
