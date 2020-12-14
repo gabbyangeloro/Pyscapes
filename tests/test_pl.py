a51a816f18d6728c684e755f3537c80f95e1097b
catanzaromj <catanzaromj@gmail.com>
2020-12-14 14:38:41 -0500

    Update tests

--- tests/test_pl.py
+++ tests/test_pl.py
@@ -1,15 +1,16 @@
-#!/usr/bin/env python3
+    #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Unit test for PersistenceLandscapeExact
 """
 
-import unittest 
+# import unittest 
+import pytest
 import numpy as np
 
 from PersistenceLandscapeExact import PersistenceLandscapeExact
 
-class TestPersistenceLandscapeExact(unittest.TestCase):
+class TestPersistenceLandscapeExact():
     
 #    def setUp(self):
 #        self.diagrams = [np.array([[1.0, 5.0], [2.0, 8.0], 
@@ -37,15 +38,15 @@ class TestPersistenceLandscapeExact(unittest.TestCase):
         Q.compute_landscape()
         
         
-        self.assertEqual(P.critical_pairs, [[[1.0, 0],[3.0, 2.0],
+        assert P.critical_pairs == [[[1.0, 0],[3.0, 2.0],
         [3.5, 1.5],[5.0, 3.0],[6.5, 1.5],[7.0, 2.0],[9.0, 0]],
         [[2.0, 0], [3.5, 1.5], [5.0, 0], [6.5, 1.5], [8.0, 0]],
         [[3.0, 0], [3.5, 0.5], [4.0, 0], [6.0, 0], [6.5, 0.5],
-        [7.0, 0]]])
+        [7.0, 0]]]
         
-        self.assertEqual(Q.critical_pairs, [[[1, 0], [3.0, 2.0], [4.0, 1.0], 
+        assert Q.critical_pairs == [[[1, 0], [3.0, 2.0], [4.0, 1.0], 
         [4.5, 1.5], [6, 0]],[[1, 0], [3.0, 2.0], [4.0, 1.0], [4.5, 1.5], 
-        [6, 0]],[[3, 0], [4.0, 1.0], [5, 0]]])
+        [6, 0]],[[3, 0], [4.0, 1.0], [5, 0]]]
         
     def test_pl_hom_degree(self):
         """
@@ -55,7 +56,7 @@ class TestPersistenceLandscapeExact(unittest.TestCase):
             dgms=[np.array([[1.0, 5.0], [2.0, 8.0], [3.0, 4.0], [5.0, 9.0],
                                 [6.0, 7.0]])],
             hom_deg=0)
-        self.assertEqual(P.hom_deg,0)
+        assert P.hom_deg == 0
         
     def test_p_norm(self):
         """
@@ -67,18 +68,18 @@ class TestPersistenceLandscapeExact(unittest.TestCase):
         negP = PersistenceLandscapeExact(
             critical_pairs=[[[0, 0], [1, -1], [2, -1], [3, -1], [4, 0]]],
             hom_deg=0)
-        self.assertEqual(P.sup_norm(), 1)
-        self.assertAlmostEqual(P.p_norm(p=2), np.sqrt(2 + (2.0/3.0)))
-        self.assertAlmostEqual(P.p_norm(p=5), (2 + (1.0/3.0))**(1.0/5.0))
-        self.assertAlmostEqual(P.p_norm(p=113), (2+ (1.0/57.0))**(1.0/113.0))
-        self.assertEqual(negP.sup_norm(), 1)
-        self.assertAlmostEqual(negP.p_norm(p=2), np.sqrt(2 + (2.0/3.0)))
-        self.assertAlmostEqual(negP.p_norm(p=5), (2 + (1.0/3.0))**(1.0/5.0))
-        self.assertAlmostEqual(negP.p_norm(p=113), (2+ (1.0/57.0))**(1.0/113.0))
+        assert P.sup_norm() == 1
+        assert P.p_norm(p=2) == pytest.approx(np.sqrt(2 + (2.0/3.0)))
+        assert P.p_norm(p=5) == pytest.approx((2 + (1.0/3.0))**(1.0/5.0))
+        assert P.p_norm(p=113) == pytest.approx((2+ (1.0/57.0))**(1.0/113.0))
+        assert negP.sup_norm() == 1
+        assert negP.p_norm(p=2) == pytest.approx(np.sqrt(2 + (2.0/3.0)))
+        assert negP.p_norm(p=5) == pytest.approx((2 + (1.0/3.0))**(1.0/5.0))
+        assert negP.p_norm(p=113) == pytest.approx((2+ (1.0/57.0))**(1.0/113.0))
         
         
 
-class PersistenceLandscapeExactBad(unittest.TestCase):
+class PersistenceLandscapeExactBad():
     """ Test bad/error test cases here.
     # TODO
     """
@@ -97,6 +98,6 @@ class PersistenceLandscapeExactBad(unittest.TestCase):
     #     self.assertAlmostEqual(P.p_norm(p=2), np.sqrt(2 + (2.0/3.0)))
     #     self.assertAlmostEqual(P.p_norm(p=5), (2 + (1.0/3.0))**(1.0/5.0))
 
-if __name__ == '__main__':
-    unittest.main()
+#if __name__ == '__main__':
+#    pytest
         
