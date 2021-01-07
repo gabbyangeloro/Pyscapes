839c4d9f8923054cf2a2e48d37952c69f928aeb8
catanzaromj <catanzaromj@gmail.com>
2021-01-07 09:01:11 -0600

    Update pl_transformer.py

--- pl_transformer.py
+++ pl_transformer.py
@@ -2,22 +2,23 @@
     Implementation of scikit-learn transformers for persistence
     landscapes.
 """
-
+from operator import itemgetter
 from sklearn.base import BaseEstimator, TransformerMixin
 from PersistenceLandscapeExact import PersLandscapeExact
+from PersistenceLandscapeGrid import PersLandscapeApprox
 
 
 # To get the following functionality to work, I think we need 
 # to split off the compute landscape function from within the 
 # PL class.
 
-class PL_exact(BaseEstimator, TransformerMixin):
+class PLE(BaseEstimator, TransformerMixin):
     """ A scikit-learn transformer class for exact persistence landscapes. The transform
     method returns the list of critical pairs for the landscape. For a vectorized
     encoding of the landscape, using the PL_grid transformer.
     """
-    def __init__(self,homological_degree:int = 0):
-        self.homological_degree = homological_degree
+    def __init__(self,hom_deg:int = 0):
+        self.hom_deg = hom_deg
         
     def fit(self,X, y=None):
         return self
@@ -25,18 +26,29 @@ class PL_exact(BaseEstimator, TransformerMixin):
     def transform(self, X, y=None):
         # Add check that X is the output of a PH calculation.
         # return X[self.homological_degree].compute_landscape()
-        result = PersLandscapeExact(diagrams=X, homological_degree=self.homological_degree)
-        result.compute_landscape()
+        result = PersLandscapeExact(dgms=X, hom_deg=self.hom_deg)
         return result.critical_pairs
     
-class PL_grid(BaseEstimator, TransformerMixin):
+class PLA(BaseEstimator, TransformerMixin):
     """ A scikit-learn transformer for grid persistence landscapes.
     """
-    def __init__(self,homological_degree:int = 0):
-        self.homological_degree = homological_degree
+    def __init__(self,hom_deg:int = 0,  start: float = None, stop: float = None, num_steps: int = 500):
+        self.hom_deg = hom_deg
+        self.start = start
+        self.stop = stop
+        self.num_steps = num_steps
         
     def fit(self,X,y=None):
         return self
     
     def transform(self,X,y=None):
-        return X[self.homological_degree].compute_landscape()
+        if self.start is None:
+            _start = min(X, key=itemgetter(0))[0]
+        else:
+            _start = self.start
+        if self.stop is None:
+            _stop = max(X, key=itemgetter(1))[1]        
+        else:
+            _stop = self.stop
+        result = PersLandscapeApprox(diagrams=X, start = _start, stop = _stop, num_steps = self.num_steps, hom_deg = self.hom_deg)
+        return result.values
