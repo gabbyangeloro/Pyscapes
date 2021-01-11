432e6cc1f38cc0cb5c81e67349b5975d80136b51
catanzaromj <catanzaromj@gmail.com>
2021-01-10 20:28:26 -0600

    Finally fix spikes

--- spike_fix.py
+++ spike_fix.py
@@ -9,15 +9,12 @@ Created on Fri Jan  8 11:17:01 2021
 from tadasets import torus
 from ripser import ripser
 from PersistenceLandscapeGrid import PersLandscapeApprox
-from PersistenceLandscapeExact import PersLandscapeExact
-from visualization import plot_landscape_simple
+from visualization import plot_landscape
 
 t = torus()
 
 tph = ripser(t)['dgms']
 
-tpla = PersLandscapeApprox(dgms=tph,hom_deg=1,num_steps=5000)
-tple = PersLandscapeExact(dgms=tph,hom_deg=1)
+tpl = PersLandscapeApprox(dgms=tph,hom_deg=1)
 #%%
-plot_landscape_simple(tple)
-plot_landscape_simple(tpla)
+plot_landscape(tpl)
